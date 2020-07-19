31f9321c0794bc41008ad6750cd74d166497a039
afteralec <github email address>
2020-07-19 16:54:44 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  case array.length
+    when 1
+      return array.join("")
+    when 2
+      return array.join(" and ")
+  end
 
+  last = ", and " + array.pop
+  
+  array.join(", ") + last
 end
